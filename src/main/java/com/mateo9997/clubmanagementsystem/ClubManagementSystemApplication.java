2da75c97116a2dd407a8e585d3fbbe74f31eb8a0
mateo9997 <mj@mateoramon.com>
2024-04-22 12:43:11 +0200

    Initial Project

---
+++ src/main/java/com/mateo9997/clubmanagementsystem/ClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mateo9997.clubmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateo9997/clubmanagementsystem/ClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateo9997.clubmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
