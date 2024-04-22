28d87b07b51831efb6d4e94e7d893be79caba34e
mateo9997 <mj@mateoramon.com>
2024-04-22 23:32:50 +0200

    Basic repository values

--- src/main/java/com/mateo9997/clubmanagementsystem/repository/PlayerRepository.java
+++ src/main/java/com/mateo9997/clubmanagementsystem/repository/PlayerRepository.java
@@ -2,14 +2,6 @@ package com.mateo9997.clubmanagementsystem.repository;
 
 import com.mateo9997.clubmanagementsystem.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-    List<Player> findByClubId(Long clubId);  // Find players by club ID
-
-    Player findByEmail(String email);  // Find a player by unique email
 }
