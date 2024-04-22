d6e84f1df51a8b07b517770c0cc3324300685d23
Mateo Javier Ramon Roman <66306403+mateo9997@users.noreply.github.com>
2024-04-22 23:42:03 +0200

    Revert "Functionality"

--- src/main/java/com/mateo9997/clubmanagementsystem/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mateo9997.clubmanagementsystem.repository;
-
-import com.mateo9997.clubmanagementsystem.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
