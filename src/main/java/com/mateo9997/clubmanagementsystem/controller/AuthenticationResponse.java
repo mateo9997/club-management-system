37c19d6eab7109c003351d396bc75b2bf939fa3f
mateo9997 <mj@mateoramon.com>
2024-04-23 15:24:01 +0200

    Security configuration class to set up Spring Security with JWT, utility class for handling JWT operations (creating and validating tokens), CustomUserDetailsService  class to load user-specific data and unauthorized requests, AuthenticationController class to handle login requests and issue JWTs to authenticated users, JWT Request Filter to intercept requests and set the authentication context based on the JWT and JwtAuthenticationEntryPoint to check if access without credentials were added

---
+++ src/main/java/com/mateo9997/clubmanagementsystem/controller/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.mateo9997.clubmanagementsystem.controller;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
